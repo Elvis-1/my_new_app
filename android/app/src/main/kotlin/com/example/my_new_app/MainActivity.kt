7972bdca3cfe1aaffc7d28f3cba003eee4bef519
Elvis <66243414+Elvis-1@users.noreply.github.com>
2021-09-09 10:55:48 +1300

    yet to complete

--- android/app/src/main/kotlin/com/example/my_new_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/my_new_app/MainActivity.kt
@@ -2,5 +2,14 @@ package com.example.my_new_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ImagePickerPlugin.registerWith(
+                registrarFor("io.flutter.plugins.imagepicker.ImagePickerPlugin"))
+        SqflitePlugin.registerWith(registrarFor("com.tekartik.sqflite.SqflitePlugin"))
+
+    }
+
 }
+
--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import '../widgets/location_input.dart';
 import 'package:provider/provider.dart';
 import '../providers/great_places.dart';
 import '../widgets/image_inputs.dart';
@@ -51,6 +52,8 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                       ),
                       SizedBox(height: 10,),
                       ImageInput(_selectImage),
+                      SizedBox(height: 10,),
+                      LocationInput(),
                     ],
                   ),
                 ),
